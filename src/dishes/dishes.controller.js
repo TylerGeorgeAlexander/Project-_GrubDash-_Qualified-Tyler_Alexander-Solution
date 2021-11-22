5b86f8a93c1676fd763cbe21f8e664ed93a85c6b
Tyler G Alexander <tylergeorgealexander@gmail.com>
2021-11-22 11:19:19 -0800

    examples for looping validation function

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -21,17 +21,53 @@ const nextId = require("../utils/nextId");
 }
 */
 
-// TODO
-function create(req, res) {
-  const { data: { href } = {} } = req.body;
-  const newUrls = {
-    id: urls.length + 1,
-    href,
-  };
-  urls.push(newUrls);
-  res.status(201).json({ data: newUrls });
+// Ex create
+
+function create(req, res, next) {
+  const { data } = req.body;
+  if (!data) {
+    return next({
+      status: 400,
+      message: "body must have `data` key",
+    });
+  }
 }
+
+function hasValidProperties(req, res, next) {
+  const { data } = req.body;
+  const requiredFields = ["name", "description", "image_url", "price"];
+  for (const field of requiredFields) {
+    if (!data[field]) {
+      return next({
+        status: 400,
+        message: `Dish must include ${field}`,
+      });
+    }
+    if (typeof data.price !== "number" || data.price < 1){
+        return next({
+            status: 400,
+            message: `Dish must have a price that is an integer greater than 0`,
+          });
+    }
+  }
+}
+
+// TODO
+// function create(req, res) {
+//   const { data: { href } = {} } = req.body;
+//   const newDish = {
+//     id: nextId,
+//     name,
+//     description,
+//     price,
+//     image_url,
+//   };
+//   dishes.push(newDish);
+//   res.status(201).json({ data: newDish });
+// }
+
 // Validation functions
+/*
 function nameValidation(req, res, next) {
   const { data: { name } = {} } = req.body;
 
@@ -72,30 +108,37 @@ function imageUrlValidation(req, res, next) {
   }
   next({ status: 400, message: "Dish must include a image_url" });
 }
+*/
 
 // DONE
-function read(req, res, next) {
+function read(req, res) {
   const { dishId } = req.params;
   const dishFound = dishes.find((dish) => dish.id === dishId);
   res.json({ data: dishFound });
 }
 
 // TODO
-function update(req, res) {
-  const foundUse = res.locals.use;
-  const { data: { text } = {} } = req.body;
-  foundUse.text = text;
-  res.json({ data: foundUse });
+// function update(req, res) {
+//   // const foundUse = res.locals.use;
+//   const dishId = Number(req.params.dishId);
+//   const foundUse = dishes.find((dish) => dish.id === dishId);
+//   const { data: { text } = {} } = req.body;
+//   foundUse.text = text;
+//   res.json({ data: foundUse });
+// }
+
+function update(req, res, next) {
+  res.json({});
 }
 
 // DONE
-function list(req, res, next) {
+function list(req, res) {
   res.json({ data: dishes });
 }
 
 module.exports = {
-  create: [nameValidation, descriptionValidation, priceValidation, imageUrlValidation, create],
+  create: [hasValidProperties, create],
   read,
-  update: [nameValidation, descriptionValidation, priceValidation, imageUrlValidation, update],
+  update: [hasValidProperties, update],
   list,
 };
